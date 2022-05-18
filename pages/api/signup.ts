4af622be3fc2f0757c0e9ee91a3c4637a5901c6d
Guilherme <gui.mochila@gmail.com>
2022-05-18 08:00:23 +0200

    feature: Signup added

---
+++ pages/api/signup.ts
@@ -0,0 +1,58 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import cookie from 'cookie'
+import prisma from '../../lib/prisma'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const signup = async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync()
+  const { email, password } = req.body
+
+  let user
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+      select: {
+        createdAt: true,
+        id: true,
+        email: true,
+        password: false,
+      },
+    })
+  } catch (e) {
+    res.status(401)
+    res.json({ error: 'User already exists' })
+    return
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '8h',
+    },
+  )
+
+  res.setHeader(
+    'Set-Cookie',
+    cookie.serialize('MUSICFY_ACCESS', token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60,
+      path: '/',
+      sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production',
+    }),
+  )
+
+  res.json(user)
+}
+
+export default signup
